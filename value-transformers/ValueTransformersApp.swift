3eb5b309230634cfb03cdca7b21ed8ab041aacea
Boris Proshin <kaktusao@mail.ru>
2023-09-14 13:11:23 +0300

    chore: init

---
+++ value-transformers/ValueTransformersApp.swift
@@ -0,0 +1,32 @@
+//
+//  value_transformersApp.swift
+//  value-transformers
+//
+//  Created by Boris Proshin on 14.09.2023.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ValueTransformersApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
